01a7e4f72e312ac97ad25050ea2d3924449f4357
Lukas Frick <contatolukasfrick@gmail.com>
2026-08-30 19:29:16 -0300

    chore: configura fundação do frontend (#4)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Chamados de Manutenção</h1>
+      <p>Prova de conceito para registro e acompanhamento de chamados.</p>
+    </main>
+  )
+}
+
+export default App
